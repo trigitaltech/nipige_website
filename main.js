a1265ce44989fe54b63371f8e98d0638f22bca6b
naziyathedev <naziyanaz1208@gmail.com>
2021-12-02 17:18:18 +0530

    fix:responsiveness

--- main.js
+++ main.js
@@ -20,3 +20,70 @@ window.addEventListener('click', function (e) {
     }
 
 })
+
+
+// p tags
+const options = document.querySelectorAll(".left-bar > p");
+const elements = document.querySelectorAll(".right-tab > div.card");
+
+options.forEach(element => {
+    element.addEventListener("click", e => {
+        classname = e.target.className.split("-");
+        classname.pop();
+
+        // Make all hidden
+        hide();
+        e.target.classList.add("active");
+        elem = document.querySelector(`.right-tab > div.${classname}-card`);
+        elem.classList.toggle("hidden");
+        
+    })
+})
+
+
+const hide = () => {
+    elements.forEach(e => {
+        e.classList.add("hidden");
+    });
+
+    options.forEach(e => {
+        e.classList.remove("active");
+    })
+
+}
+
+
+ function clickClickHandler(){
+     console.log("clicked");
+     document.getElementById("#toggleMobileMenu").style.display="block";
+     }
+
+   function responsive_menu(){
+ var e = document.getElementById('menu_small');
+ if(e.className === 'menu-item'){
+    e.className += " responsive";
+ }else{
+     e.className === 'menu-item';
+ }
+   }  
+
+
+    //  For second page
+    const optionsList = document.querySelectorAll(".options-list > div.list");
+    const rightOptions = document.querySelectorAll(".option");
+    // console.log(rightOptions[0].firstElementChild.getAttribute("src"))
+    const hideOptions = () => {
+        rightOptions.forEach(e => e.classList.add("hidden"));
+        optionsList.forEach(e => e.firstElementChild.classList.remove("activeOption"));
+    }
+    optionsList.forEach(element => {
+        element.addEventListener("click", (e) => {
+            const name = e.target.innerHTML.split(" ").join("-").toLowerCase();
+            hideOptions();
+
+            document.querySelector(`.${name}`).classList.toggle("hidden");
+            e.target.classList.add("activeOption");
+        })
+    });
+
+    
