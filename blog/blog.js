a7eff2ea52efa7ec95740b5b55d7095f5ff5b7b4
naziyathedev <naziyanaz1208@gmail.com>
2021-12-09 00:33:33 +0530

    updated:blog

---
+++ blog/blog.js
@@ -0,0 +1,112 @@
+const data = [
+    {
+        "id": "1",
+        "para": "Nipige - A Multi-Vendor Online Selling Platform",
+        "author": "Pavan",
+        "imgLink": "abs_blogs_045"
+    },
+    {
+        "id": "2",
+        "para": "The Benefits of a Resilient Digital Enablement &amp; Multiple Business Deliverance Platform.",
+        "author": "Pavan",
+        "imgLink": "img2"
+    },
+    {
+        "id": "3",
+        "para": "The Role &amp; Essence of Digitalization for Business Conduct",
+        "author": "Pavan",
+        "imgLink": "img3"
+    },
+    {
+        "id": "4",
+        "para": "Kalam Rentals &amp; Asset Management Services.",
+        "author": "Pavan",
+        "imgLink": "img4"
+    },
+    {
+        "id": "5",
+        "para": "Online Hyperlocal markets are the future of local business deliverance",
+        "author": "Pavan",
+        "imgLink": "img5"
+    },
+    {
+        "id": "6",
+        "para": "Multi-vendor online selling platforms and e-market places could change the prospects of small-time businesse.",
+        "author": "Pavan",
+        "imgLink": "img6"
+    },
+    {
+        "id": "7",
+        "para": "Business visibility and customer access are better through e-market places",
+        "author": "Pavan",
+        "imgLink": "img7"
+    },
+    {
+        "id": "8",
+        "para": "A market for all is transforming the concept of online buying and selling",
+        "author": "Pavan",
+        "imgLink": "img8"
+    }
+]
+
+
+
+const searchBlog = (e) => {
+    let filter = document.querySelector('#myInput').value.toLowerCase();
+    let resultDiv = document.querySelector(".container.card_container.mt-5");
+
+    const sectionDiv = document.querySelector("section > .container")
+    sectionDiv.removeChild(resultDiv);
+
+    let cards = data;
+    let match = [];
+
+    for (let x = 0; x < cards.length; x++) {
+        if (cards[x].para.toLowerCase().includes(filter)) {
+            match.push(cards[x]);
+        }
+    }
+
+    if (match.length > 0) {
+        displayCards(match);
+    } else {
+        displayCards(cards);
+    }
+}
+
+const displayCards = (arr) => {
+    const container = document.querySelector("section > .container");
+
+    const newNode = document.createElement("div");
+    newNode.setAttribute("class", "container card_container mt-5");
+
+    arr.forEach((elem, index) => {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("card1");
+
+        newDiv.innerHTML += `
+            <img class="card_img" src="img/${elem.imgLink}.jpg">
+            <div class="card">
+            <div class="content">
+                <a href="card${index+1}.html">
+                <div class="card__title">
+                    <p id="myUL">${elem.para}</p>
+                </div>
+                <div class="card__description">
+                    <p>Read More</p>
+                </div>
+                </a>
+                <div class="card__footer">
+                <div class="author"> Published By ${elem.author}</div>
+                </div>
+            </div>
+            </div>
+        `
+
+        newNode.appendChild(newDiv);
+    })
+
+    container.appendChild(newNode);
+}
+
+displayCards(data);
